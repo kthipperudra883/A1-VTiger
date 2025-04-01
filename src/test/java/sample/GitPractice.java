dae6331b46210074eacf292246db85d4baecfc31
Rudra <k.thipperudra883@gmail.com>
2025-04-01 16:46:53 +0530

    branch commit

---
+++ src/test/java/sample/GitPractice.java
@@ -0,0 +1,17 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class GitPractice {
+	public class Demo {
+		@Test
+		public void method()
+		{
+			System.out.println("pushed from Git branch");
+			
+		}
+	}
+	
+}
+		
+
